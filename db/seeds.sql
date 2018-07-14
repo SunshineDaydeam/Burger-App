bb32806a9f1046fd8390f8afa72c6fe1f8b5c942
Sunshine Daydream <alexbruner@live.com>
2018-07-13 23:31:15 -0500

    added stuff?

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers(burger_name, devoured)
-VALUES("RyeMCA Burger", true), ("Baby You can chive my car burger", false), ("plain cheeseburger",false);
+VALUES("Tofu Lettuce Wrap", true), ("Black Bean Bunless Burger", false), ("Hamburgueso con queso",false);
--- public/assets/java/userJava.js
+++ public/assets/java/userJava.js
@@ -1,60 +1,61 @@
 $(document).ready(function() {
 
-    //Add Burger Click Handler
-    $(".create-form").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-    
-        var newBurger = {
-          burger_name: $("#burger-input").val().trim(),
-          devoured: "false"
-        };
+  //Add Burger Click Handler
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
 
-        console.log(newBurger);
-    
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
-        }).then(
-          function() {
-            console.log("created new burger");
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
-      });
+    var newBurger = {
+      burger_name: $("#burger-input").val().trim(),
+      devoured: "false"
+    };
 
-      $(".change-devoured").on("click", function(event) {
-        var id = $(this).data("id");
-        var newBurgerStatus = $(this).data("buttondevoured");
-        var newBurgerState;
-        if (newBurgerStatus == 0){
-          newBurgerState = {
-            devoured: 1
-          };
-        }
-        else{
-          newBurgerState = {
-            devoured: 0
-          };
-        }
-          
-        
-        console.log(newBurgerState)
-    
-        //Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-          data: newBurgerState
-        }).then(
-          function() {
-            console.log("changed devoured to", newBurgerState);
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
-      });
+    console.log(newBurger);
+
+    //ajax post
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newBurgerStatus = $(this).data("buttondevoured");
+    var newBurgerState;
+    if (newBurgerStatus == 0){
+      newBurgerState = {
+        devoured: 1
+      };
+    }
+    else{
+      newBurgerState = {
+        devoured: 0
+      };
+    } 
+    console.log(newBurgerState)
+    //ajax put
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(
+      function() {
+        console.log("changed devoured to", newBurgerState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  // $(".delete-burger").on("click", function(event){
+  //   var id = $(this).data("id");
+  // })
 
 
 })
